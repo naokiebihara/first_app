57b6f38165aa7741b026eb1e5677b7ce50fdbdb5
naokiebihara <102887633+naokiebihara@users.noreply.github.com>
2022-04-03 16:26:18 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,4 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
-
-  def new
-  end
-
-  def create
-    post.create(content: params[:content])
   end
 end
-
